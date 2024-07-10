8b24da55fd268fd904fba15a881627db113de0eb
Eloagbawe <eloagbawe@gmail.com>
2024-07-10 12:37:15 -0400

    update app routes

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+import 'dotenv/config';
+import authRouter from './routes/authRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 const port = process.env.PORT || 8080;
 
@@ -8,6 +10,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
 
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
