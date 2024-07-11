413ff5b31194d65fd70c7d876e75eff51261ad7c
Eloagbawe <eloagbawe@gmail.com>
2024-07-11 15:08:07 -0400

    update user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 export const getUsers = async (req, res) => {
-  const { query } = req.body;
+  const { query } = req.query;
 
   try {
     const users = await knex("users")
@@ -13,8 +13,8 @@ export const getUsers = async (req, res) => {
       .orWhere("first_name", "LIKE", `%${query}%`)
       .orWhere("last_name", "LIKE", `%${query}%`);
 
-    res.status(200).json(users);
+    res.status(200).json(users.filter((user) => user.id !== req.user.id));
   } catch (err) {
-    console.error(err);
+    res.status(500).json({ message: "An error has occurred on the server" });
   }
 };
