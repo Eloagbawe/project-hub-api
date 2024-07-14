dc9547f26c553e0f9b5d5f576c5a6b5ea6e542ab
Eloagbawe <eloagbawe@gmail.com>
2024-07-14 16:32:08 -0400

    update get users function

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ import configuration from "../knexfile.js";
 const knex = initKnex(configuration);
 
 export const getUsers = async (req, res) => {
-  const { query } = req.query;
+  const { query, project_id } = req.query;
 
   try {
     const users = await knex("users")
@@ -13,7 +13,19 @@ export const getUsers = async (req, res) => {
       .orWhere("first_name", "LIKE", `%${query}%`)
       .orWhere("last_name", "LIKE", `%${query}%`);
 
-    res.status(200).json(users.filter((user) => user.id !== req.user.id));
+    let output = [...users];
+
+    if (project_id) {
+      const user_projects = await knex("user_projects")
+        .select("user_id")
+        .where({ project_id });
+
+      const teamMembers = user_projects.map((item) => item.user_id);
+
+      output = output.filter((x) => !teamMembers.includes(x.id));
+    }
+
+    res.status(200).json(output.filter((user) => user.id !== req.user.id));
   } catch (err) {
     res.status(500).json({ message: "An error has occurred on the server" });
   }
